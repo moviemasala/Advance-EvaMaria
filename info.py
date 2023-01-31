b259762cc62acc7a36071a3d5954cc5c241bd59a
moviemasala <118058892+moviemasala@users.noreply.github.com>
2023-01-31 08:42:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,7 +23,7 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/37a222dc67679f091b188.jpg')
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '995932132').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001742616778  -1001680841206  -1001614580625  -100341824429').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001742616778  -1001680841206  -1001614580625  -100341824429 -1001551361967').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL', '-1001614580625')
